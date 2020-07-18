6a8b999f191fc49926a8e5a582a1a31b7172d48c
Luis Adame <luis@luisadame.ninja>
2020-07-18 22:49:21 +0200

    change metadata

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { format, startOfMonth, addDays, subDays } from "date-fns";
 import domToImage from "dom-to-image";
